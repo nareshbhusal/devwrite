88de00fb27ca32013843da8540731c018a5aee1f
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-01 18:10:46 +0530

    Create Post model

---
+++ server/src/models/Post.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const user = db.define('post', {
+    title: {
+        type: Sequelize.STRING
+    },
+    body: {
+        type: Sequelize.STRING
+    },
+    user: {
+        type: Sequelize.INTEGER
+    },
+    about: {
+        type: Sequelize.STRING
+    },
+    tags: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    comments: {
+        // json with objects users and their comments and time of commenting
+        type: Sequelize.JSON
+    },
+    createdAt: {
+        type: Sequelize.INTEGER
+    },
+    editedAt: {
+        type: Sequelize.INTEGER
+    },
+    likedBy: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER)
+    }
+
+})
+
+module.exports = user;
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize');
 const db = require('../config/database');
 
 const user = db.define('user', {
+    id: {
+        type: Sequelize.INTEGER
+    },
     name: {
         type: Sequelize.STRING
     },
@@ -16,6 +19,18 @@ const user = db.define('user', {
     },
     posts: {
         // array of posts' ids
+        type: Sequelize.ARRAY(Sequelize.INTEGER)
+    },
+    createdAt: {
+        type: Sequelize.INTEGER
+    },
+    folowers: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER)
+    },
+    following: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER)
+    },
+    likedPosts: {
         type: Sequelize.ARRAY(Sequelize.INTEGER)
     }
 })
