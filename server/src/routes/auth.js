4828cc1e41513d425acd24474978d755dfb00caf
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-01 22:02:02 +0530

    Add create post route

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,6 +2,38 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+const updateSessions = async (user) => {
+
+    // takes the user fetched from the database freshly,
+    // and update it's session_ids 
+    // logic -- keep number of session id below 5
+
+    let session_ids;
+    session_ids = user.session_ids;
+    if (session_ids) {
+        session_ids = session_ids.split(',');
+        if (session_ids.length > 4) {
+            session_ids.splice(4, 1, req.session.sessionID);
+            session_ids = session_ids.toString();
+        }
+    }
+
+    const updatedUserValues = user;
+    updatedUserValues.session_ids = session_ids;
+    try {
+        await User.update(
+            { ...updatedUserValues },
+            {
+                where: {
+                id: updatedUserValues.id
+            }
+        }
+        );
+    } catch(err) {
+        console.log(err);
+    }
+}
+
 // post
 router.get('/login', async (req, res) => {
     // const user = {...req.body};
@@ -20,7 +52,8 @@ router.get('/login', async (req, res) => {
                 ...user
             }
         });
-        if (userInRecords) {
+        if (userInRecords) {           
+            await updateSessions();
             // login successful
             console.log('loggedin', userInRecords.name);
             return res.send('logged in successfully!')
--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,13 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
 
-router.get('/', async (req, res) => {
-    res.send('posts');
-})
+// router.get('/', async (req, res) => {
+//     try {
+//         const posts = await Post.findAll();
+//         if (!posts.length) {
+//             return res.send('404. No posts found!');
+//         }
+//         return posts;
+//     } catch(err) {
+//         console.log(err);
+//     }
+// })
 
 // Create a post
-router.post('/', async (req, res) => {
-    res.send('create post');
+//post
+router.get('/', async (req, res) => {
+    //const post = req.body || {};
+    let post = { ...req.query };
+    post.user = req.session.user.id;
+    const errors = []
+    if (!post.title, !post.about, !post.body) {
+        errors.push({ err: 'Please fill in all field!' });
+        return res.send(errors);
+    }
+    try {
+        post = { ...req.query };
+        post.user = req.session.user.id;
+        post = await Post.create(post);
+        console.log('CREATED THE POST');
+        // get the user
+        const user = await User.findOne({
+            where: {
+                id: req.session.user.id
+            }
+        })
+        console.log('FOUND THE USER');
+        // update the posts id column in the database on user table
+        let posts = user.posts || [];
+        posts.push(parseInt(post.id));
+        console.log(posts);
+
+        await User.update(
+            { 
+                posts 
+            },
+            {
+                where: {
+                id: req.session.user.id
+            }
+        }
+        );
+        return res.status(200).send({ msg: 'Done!' });
+    } catch(err) {
+        console.log(err);
+        return res.send('Something went wrong!')
+    }
 })
 
 // Get a particular post by its id
