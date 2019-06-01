b8dc2ee885a14fe2de1c096168c1280623519b15
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-01 15:37:52 +0530

    Add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const redis = require('redis');
 const RedisStore = require('connect-redis')(session);
 
+const cors = require('cors');
 const db = require('./config/database');
 
 const app = express();
@@ -17,6 +18,27 @@ client.on('connet', () => {
 // configure middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: true }));
+
+// Set up session with redis client
+app.use(session({
+    genid: function(req) {
+        return uuid(); //use UUIDs for session IDs
+    },
+    secret: 'ssssshhhh',
+    saveUninitialized: true,
+    resave: true,
+    store: new RedisStore({ client }),
+    cookie: {
+        secure: false,
+        maxAge: 30 * 24 * 60 * 1000 // 30 days
+    }
+}));
+
+// Initialize database
+db.authenticate()
+    .then(() => console.log('database connected'))
+    .catch(err => console.log('SOMETHING WRONG WITH DATABASE', err));
 
 const port = process.env.PORT || 5000;
 
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,1 +1,15 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('mediumdb', 'postgres', 'nnnsss333', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+        max:5,
+        min: 0,
+        acquire: 30000,
+        idle: 1000
+    },
+    define : {
+        timestamps: false
+    }
+})
