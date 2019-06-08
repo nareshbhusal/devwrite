0520be95c7901ca07a71a88c335f41bb00ffe0dd
nareshsharma123 <nareshsharma123@gmail.com>
2019-06-08 10:29:57 +0530

    Add logout route handler

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -24,8 +24,8 @@ router.get('/login', async (req, res) => {
             await updateSessionIDs(userInRecords, req.sessionID);
             // set user on cookie
             addCookie(req, userInRecords);
-            
             return res.send({ msg: 'Logged in!' });
+
         } else {
             // creds don't match
             errors.push({ err: 'Wrong password or username!' })
@@ -39,7 +39,36 @@ router.get('/login', async (req, res) => {
 });
 
 router.get('/logout', async (req, res) => {
-    res.send('logout');
+    // Clear session_id from the database
+    if (req.session.user) {
+        try{
+            const userId = req.session.user.id;
+            const user = await getUser({ id: userId });
+            if (user) {
+                let session_ids = user.session_ids;
+                session_ids = session_ids.split(',').filter(session_id => {
+                    return session_id !== req.sessionID
+                }).toString();
+
+                User.update({
+                    session_ids
+                })
+                req.session.destroy((err) => {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    res.redirect('/');
+                });
+            } else {
+                console.log(err);
+                throw "user not found server error"
+            }
+        } catch(err) {
+            res.status(500).send('something went wrong while trying to log out!')
+        }
+    } else {
+        res.status(400).send('You\'re aready logged out');
+    }
 })
 
 module.exports = router;
