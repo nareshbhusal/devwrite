cdf6c69bb3bf724d5667e69d1c3c161d0e1fa7cd
nareshsharma123 <nareshsharma123@gmail.com>
2019-11-27 11:36:26 +0530

    Add https to express

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,27 @@
 const app = require('./index');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`listening at port ${port}`);
-})
+const { PORT, SECURE } = process.env;
+
+const port = PORT || 5000;
+const certPath = path.join(__dirname, '../src/certs');
+
+if (SECURE) {
+    const key = fs.readFileSync(certPath + '/selfsigned.key');
+    const cert = fs.readFileSync(certPath + '/selfsigned.crt');
+    const options = {
+        key,
+        cert
+    }
+    const server = https.createServer(options, app);
+
+    server.listen(port, () => {
+        console.log(`secure server listening on port ${port}`);
+    });
+} else {
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+}
